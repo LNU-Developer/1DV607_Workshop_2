575195e52724db49c8d5d12496f8d378c28bae73
Pernilla Göth <pg222jx@student.lnu.se>
2020-09-10 09:49:18 +0200

    Adding BoatRegister

---
+++ Model/BoatRegister.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace workshop_2
+{
+    class BoatRegister
+    {
+        private List<Boat> _boats = new List<Boat>();
+
+        public void addBoat(BoatTypes boatType, double length)
+        {
+
+        }
+
+        public void deleteBoat()
+        {
+
+        }
+
+        public void updateBoat()
+        {
+        }
+
+    }
+}
