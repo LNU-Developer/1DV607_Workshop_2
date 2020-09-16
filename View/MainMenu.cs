29b705d9c257bb6194f63088e5f7de837296a4cd
Pernilla Göth <pg222jx@student.lnu.se>
2020-09-16 13:26:31 +0200

    Adding a bit more in MainMenu

--- View/MainMenu.cs
+++ View/MainMenu.cs
@@ -19,7 +19,18 @@ namespace workshop_2
             if(Int32.Parse(choice) == 1)
             {
                 Console.WriteLine("You choose to register member");
-            } else 
+                Console.WriteLine("Please enter first name:");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Please enter last name:");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Please enter your personal id number in 10 digits:");
+                string pId = Console.ReadLine();
+                Console.WriteLine("Are these credentials correct:");
+                Console.WriteLine("First name: " + firstName);
+                Console.WriteLine("Last name: " + lastName);
+                Console.WriteLine("Personal id number: " + pId);
+            } 
+            else 
             {
                 Console.WriteLine("Not a valid option");
             }
